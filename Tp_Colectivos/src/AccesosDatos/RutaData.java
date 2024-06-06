b16949124796d37bbeaf01feeb0111a4319a82a3
JuanDiegoCARP <familiaalonso.jda@gmail.com>
2024-06-06 11:21:29 -0300

    Metodos buscarRuta de la clase RutaData

--- Tp_Colectivos/src/AccesosDatos/RutaData.java
+++ Tp_Colectivos/src/AccesosDatos/RutaData.java
@@ -8,6 +8,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 
@@ -38,10 +41,89 @@ public class RutaData {
             JOptionPane.showMessageDialog(null, "Error al acceder al tabla Ruta "+e.getMessage());
         }        
     }
+      public List<Ruta> listarRuta(){
+      List<Ruta> rutas = new ArrayList<>();
+      String sql = "SELECT `idRuta`, `origen`, `destino`, `duracion_estimada` FROM `ruta` WHERE estado = 1 ";
+       try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){   
+               Ruta ruta=new Ruta();
+               Time duracion_estimada = Time.valueOf(ruta.getDuracion_estimadad());
+               ruta.setIdRuta(rs.getInt("idRuta"));
+               ruta.setOrigen(rs.getString("origen"));
+               ruta.setDestino(rs.getString("destino"));
+               ruta.setDuracion_estimadad(rs.getTime("duracion_estimada").toLocalTime());
+               rutas.add(ruta);                                            
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se puedo acceder a la tabla ruta");
+        }
+        return rutas; 
+      }
+        
     
-    }    
-        
-        
+    public Ruta buscarRutaPorOrigen(String origen){
+    Ruta ruta = null;
+    String sql = "SELECT `idRuta`, `origen`, `destino`, `duracion_estimada` FROM `ruta` WHERE origen = ? and estado = 1 ";
+    PreparedStatement ps = null;
+    try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1,origen );
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+            ruta=new Ruta();
+            ruta.setIdRuta(rs.getInt("idRuta"));
+            ruta.setOrigen(rs.getString("origen"));
+            ruta.setDestino(rs.getString("destino"));
+            ruta.setDuracion_estimadad(rs.getTime("duracion_estimada").toLocalTime());
+            ruta.setEstado(true);
+
+            } else {
+             JOptionPane.showMessageDialog(null, "No existe la ruta");
+            }
+            ps.close();
+            } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Ruta "+ex.getMessage());
+            }
+    return ruta;
+}
+
+public Ruta buscarRutaPorDestino(String destino){
+    Ruta ruta = null;
+    String sql = "SELECT `idRuta`, `origen`, `destino`, `duracion_estimada` FROM `ruta` WHERE destino = ? and estado = 1 ";
+    PreparedStatement ps = null;
+    try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1,destino );
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+            ruta=new Ruta();
+            ruta.setIdRuta(rs.getInt("idRuta"));
+            ruta.setOrigen(rs.getString("origen"));
+            ruta.setDestino(rs.getString("destino"));
+            ruta.setDuracion_estimadad(rs.getTime("duracion_estimada").toLocalTime());
+            ruta.setEstado(true);
+
+            } else {
+             JOptionPane.showMessageDialog(null, "No existe la ruta");
+            }
+            ps.close();
+            } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Ruta "+ex.getMessage());
+            }
+    return ruta;
+    
+
+}
+
+}
+
+
+    
         
         
         
